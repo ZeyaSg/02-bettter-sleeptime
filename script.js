4a004b60397b1b030bccc49085d8617320afa449
ZeyaSg <zsong30@jhu.edu>
2022-09-09 23:32:26 -0400

    get user selected time

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const colors = [
-  "white",
-  "red",
-  "white",
-  "red",
-  "white",
-  "red",
-]
+    "rgb(168, 39, 254)",
+    "rgb(154, 3, 254)",
+    "rgb(150, 105, 254)",
+    "rgb(140, 140, 255)",
+    "rgb(187, 187, 255)",
+    "rgb(143, 254, 221)",
+  ]
 
 function handleOnClick() {
     let output = document.querySelector(".output");
@@ -13,10 +13,16 @@ function handleOnClick() {
   
     let hours = document.getElementById("hours");
     hours.innerHTML = "";
+
+    let hh = document.getElementById("hh").value;
+    let mm = document.getElementById("mm").value;
+    let ampm = document.getElementById("ampm").value;
+    hh = ampm === "PM" ? Number.parseInt(hh) + 12 : hh;
   
     let now = new Date();
-    // allow 14 minutes to fall sleep!
-    now.setMinutes(now.getMinutes() + 14);
+
+    now.setHours(hh);
+    now.setMinutes(mm);
   
     // calculate sleep cycles!
     for (let i = 1; i <= 6; i++) {
